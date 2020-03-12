c8633b0ee636a40dd34761c7b138d034c9ef0eb9
shenyn <shenyn@eetrust.com>
2020-03-12 16:17:38 +0800

    优化周报爬虫，命令行page参数和YAML文件的最近推送时间只有一个参数生效

--- Python/weekly.py
+++ Python/weekly.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import re
 import yaml
 import argparse
@@ -7,10 +9,10 @@ from enum import Enum
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-# ================== 命令行输入参数，默认值：1 ======================== #
+# ================== 命令行输入参数，默认值：0 ======================== #
 
 parser = argparse.ArgumentParser(usage='参数说明')
-parser.add_argument('-p', dest='page', type=int, help='共查询几页提交记录', default=2)
+parser.add_argument('-p', dest='page', type=int, help='共查询几页提交记录, page > 0 以page数为准, page <= 0 以YAML文件中的最近推送时间为准', default=0)
 args = parser.parse_args()
 
 # ================== 可修改数据 ======================== #
@@ -33,7 +35,7 @@ LOGIN_URL = f"{BASE_URL}/user/login"
 
 # 更多路径，用于点击更多按钮
 more_path = ""
-# 最大的推送时间
+# 最近推送时间
 last_push_time = ""
 
 s = requests.Session()
@@ -83,30 +85,39 @@ def build_dict(pro_type):
 
 # 根据项目分组数据
 def grouped_data(new_divs):
+    # 最近推送时间
+    global last_push_time
+    # 结果集
     dict_arr = []
-    # print(new_divs)
+    # 是否停止,当推送内容的时间小于上次记录的推送时间则停止
+    stop_flag = False
 
     for new_divs_str in new_divs:
+        # 获取工程名称
         soup = BeautifulSoup(str(new_divs_str), "html.parser")
         a_arr = soup.p.find_all("a")
         pro_name = a_arr[-1].string
 
-        # 核对时间,如提交时间小于记录时间则break
-        time_flag = False
-        push_time_spans = soup.find_all(attrs={'class': 'time-since'})
-        for span in push_time_spans:
-            push_time = span.attrs["title"]
+        # 命令行未设置配置参数则以yaml文件记录的时间为准
+        if SHOW_PAGE_TOTAL == 0:
+            # 核对时间,如提交时间小于记录时间则break
+            ps = soup.find_all("p")
+            span_attrs = ps[-1].span.attrs
+            push_time = ''
+            if 'title' in span_attrs:
+                push_time = ps[-1].span.attrs["title"]
+            else:
+                push_time = ps[-1].span.attrs["data-content"]
+
             old_push_time = get_yaml_value("push_time")
 
             # 记录最大的时间
             if last_push_time == "":
-            	last_push_time = push_time
+                last_push_time = push_time
 
             if datetime.strptime(push_time, GMT_FORMAT) < datetime.strptime(old_push_time, GMT_FORMAT):
-	            time_flag = True
-
-        if time_flag:
-            break
+                stop_flag = True
+                break
 
         # 找出用户push的内容
         push_lis = soup.find_all("li")
@@ -137,28 +148,36 @@ def grouped_data(new_divs):
 
             dict_arr.append(push_dict)
 
-    return dict_arr
+    return dict_arr, stop_flag
 
 
 # 显示push内容
 def show_push_content():
     dict_arr = []
 
-    for index in range(0, SHOW_PAGE_TOTAL):
+    # SHOW_PAGE_TOTAL == 0时 ，默认5页，以最近推送时间为准
+    page = 5
+    if SHOW_PAGE_TOTAL > 0:
+        page = SHOW_PAGE_TOTAL
+
+    for index in range(0, page):
         # 第一个请求用于登录并获取第一页数据
         if index == 0:
-        	# 登录数据
-        	username = get_yaml_value("username")
-        	password = get_yaml_value("password")
-        	login_data = { "user_name": username, "password": password }
+            # 登录数据
+            username = get_yaml_value("username")
+            password = get_yaml_value("password")
+            login_data = { "user_name": username, "password": password }
 
-        	arr = get_push_content(LOGIN_URL, login_data)
+            r_tuple = get_push_content(LOGIN_URL, login_data)
         else:
-            arr = get_push_content(get_more_url(more_path), "", True)
+            r_tuple = get_push_content(get_more_url(more_path), "", True)
 
-        dict_arr = dict_arr + arr
+        dict_arr = dict_arr + r_tuple[0]
 
-	# 获取项目中文名与项目工程名的键值对
+        if r_tuple[1]:
+            break
+
+    # 获取项目中文名与项目工程名的键值对
     pro_type = get_yaml_value("pro_type")
 
     # 合并多次请求结果
@@ -178,10 +197,9 @@ def show_push_content():
                     for index, content in enumerate(v):
                         print(f"{index + 1}. {content}")
 
-    # 有输出时，则记录最后的推送时间，下去爬取日志时取此时间之后的推送内容
-    if len(dict_arr) > 0:
-    	print("时间：" + last_push_time)
-    	set_push_time(last_push_time)
+    # 有输出且命令行未设置page参数，则记录最后的推送时间，下去爬取日志时取此时间之后的推送内容
+    if len(dict_arr) > 0 and SHOW_PAGE_TOTAL == 0:
+        set_push_time(last_push_time)
 
 
 # 获取yaml文件的value (暂无考虑出现异常的情况)
@@ -193,10 +211,10 @@ def get_yaml_value(the_key):
             return doc["company_git"][the_key]
 
     # 如无时间则返回 2020-03-08 17:47:32
-    # return "Mon, 24 Feb 2020 15:47:04 CST"
     return "Sun, 08 Mar 2020 17:47:32 CST"
 
 
+# 设置最近推送时间
 def set_push_time(time):
     with open(YAML_FILE_PAHTH, encoding='utf-8') as f:
         doc = yaml.safe_load(f)
@@ -207,10 +225,6 @@ def set_push_time(time):
     # 保存修改
     with open(YAML_FILE_PAHTH, 'w', encoding='utf-8') as f:
         yaml.safe_dump(doc, f, default_flow_style=False)
-        # print(doc)
 
-# 项目类型
-# PRO_TYPE = get_yaml_value("pro_type")
-# print(build_dict())
+
 show_push_content()
-# print(type(get_yaml_value("pro_type")))
